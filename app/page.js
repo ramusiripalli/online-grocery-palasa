2cd696041cb7b8e556fa7d4f7381c15419022b79
ramu siripalli <ramsiripalli2772@gmail.com>
2024-09-22 12:19:30 +0530

    Next Js Basic Installation
    Next Js Basic Installation

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+     <div>
+      Hello
+      <Button>Subscribe</Button>
+     </div>
+    </main>
+  );
+}
